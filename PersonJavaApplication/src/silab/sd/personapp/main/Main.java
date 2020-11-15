9f655a6bb339daecf73f9d3150509e967d8ce70e
AleksandraMisic762 <am20170003@student.fon.bg.ac.rs>
2020-11-15 18:40:58 +0100

    First commit

---
+++ PersonJavaApplication/src/silab/sd/personapp/main/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package silab.sd.personapp.main;
+
+import silab.sd.personapp.view.form.MainFrm;
+
+/**
+ *
+ * @author laptop-02
+ */
+public class Main {
+    public static void main(String[] args) {
+        //new FrmPerson(null, true).setVisible(true);
+        
+        new MainFrm(null, true).setVisible(true);
+    }
+}
